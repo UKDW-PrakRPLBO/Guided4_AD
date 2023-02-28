ba5ac82d982d6c5e61402a2152dc90df5e94b40a
Antonius Rachmat <antoniusrc@users.noreply.github.com>
2023-02-28 07:17:38 +0700

    soal

--- src/main/java/com/rplbo/tabungan/Main.java
+++ src/main/java/com/rplbo/tabungan/Main.java
@@ -5,27 +5,10 @@ package com.rplbo.tabungan;
  */
 public class Main
 {
-    public static void tampil(Tabungan x){
-        System.out.println("Pemilik: "+x.getPemilik());
-        System.out.println("Saldo: "+x.getSaldo());
-    }
+
     public static void main( String[] args )
     {
-        Tabungan A = new Tabungan();
-        Tabungan B = new Tabungan();
-        A.setPemilik("A");
-        A.penyetoran(300);
-        B.setPemilik("B");
-        B.penyetoran(1200);
-        A.tampilkan();
-        B.tampilkan();
-        A.penarikan(150);
-        B.penyetoran(300);
-
-        B.penarikan(-500);
-        A.penyetoran(-1000);
-        A.tampilkan();
-        B.tampilkan();
+        
 
     }
 }
--- src/main/java/com/rplbo/tabungan/Tabungan.java
+++ src/main/java/com/rplbo/tabungan/Tabungan.java
@@ -1,39 +1,5 @@
 package com.rplbo.tabungan;
 
 public class Tabungan {
-    private int saldo;
-    private String pemilik;
-
-    public Tabungan() {
-    }
-
-    public int getSaldo(){ return saldo; }
-    public String getPemilik(){ return pemilik; }
-    public void setPemilik(String pemilik){
-        this.pemilik = pemilik;
-    }
-
-    public void penarikan(int jumlah){
-        if(jumlah > 0 && saldo>=jumlah){
-            saldo = saldo - jumlah;
-        }
-    }
-
-    public void penyetoran(int jumlah){
-        if(jumlah > 0){
-            saldo = saldo + jumlah;
-        }
-    }
-
-    public void transfer(int jumlah,Tabungan t){
-        if(saldo>=jumlah) {
-            penarikan(jumlah);
-            t.penyetoran(jumlah);
-        }
-    }
-
-    public void tampilkan(){
-        System.out.println("Nama pemilik: "+ pemilik);
-        System.out.println("Saldo: "+this.saldo);
-    }
+    
 }
